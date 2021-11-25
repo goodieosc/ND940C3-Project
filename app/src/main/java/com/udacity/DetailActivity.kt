b160bcdc76b64cf502606593af6f311bb2122fb6
oscar <oscar.goodwin@gmail.com>
2021-11-25 17:59:05 +0800

    Pass the file ame and download status to the DetailActivity using an intent linked to the notification button. Added a function to check on the download status.

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -1,8 +1,10 @@
 package com.udacity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
@@ -10,6 +12,20 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
+
+        val fileName = intent.getStringExtra("fileName")
+        val status = intent.getStringExtra("status")
+
+        fileNameValueTextView.setText("$fileName")
+        statusValueTextView.setText("$status")
+
+
+
+        okButton.setOnClickListener {
+
+        }
+
+
     }
 
 }
--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.udacity
 
+import android.annotation.SuppressLint
 import android.app.DownloadManager
 import android.app.NotificationChannel
 import android.app.NotificationManager
@@ -14,6 +15,7 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
+import android.os.Handler
 import android.util.Log
 import android.widget.RadioButton
 import android.widget.Toast
@@ -28,14 +30,21 @@ import kotlinx.android.synthetic.main.content_main.*
 private val NOTIFICATION_ID = 0
 private val REQUEST_CODE = 0
 
+
 class MainActivity : AppCompatActivity() {
 
     private var downloadID: Long = 0
+    private var fileName: String = ""
+    private var selectedRadioButtonText = ""
+    var downloadStatus: Int = 0
+    private var totalBytes: Int = 0
 
+    private val handler: Handler = Handler()
 
     private lateinit var notificationManager: NotificationManager
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
+    private lateinit var downloadResult: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -65,7 +74,7 @@ class MainActivity : AppCompatActivity() {
             //Create Toast when download completes.
             if(id == downloadID){
                 //Toast.makeText(applicationContext,"Download completed",Toast.LENGTH_LONG).show()
-                Log.i("MainActivity","Download completed")
+                //Log.i("MainActivity","Download completed")
 
                 // Call the sendNotification() extension function with the notification message
                 notificationManager.sendNotification(CHANNEL_ID, getString(R.string.notification_description),applicationContext)
@@ -82,13 +91,17 @@ class MainActivity : AppCompatActivity() {
 
             //Set the value or URI to the content description of the selected radiobutton
             val selectedRadioButton: RadioButton = findViewById(selectedRadioButtonId)
+
+            selectedRadioButtonText = selectedRadioButton.text.toString()
+            Log.i("MainActivity", selectedRadioButtonText)
+
             val url = selectedRadioButton.contentDescription.toString()
             Log.i("MainActivity",selectedRadioButton.contentDescription.toString())
 
             //Get the file name from the URI
             val urlWithNoSpace = "${selectedRadioButton.contentDescription}".filter { !it.isWhitespace() }
             val index = urlWithNoSpace.lastIndexOf("/")
-            val fileName = urlWithNoSpace.substring(index+1)
+            fileName = urlWithNoSpace.substring(index+1)
             Log.i("MainActivity",fileName)
 
             val request =
@@ -104,8 +117,12 @@ class MainActivity : AppCompatActivity() {
             val downloadManager = getSystemService(DOWNLOAD_SERVICE) as DownloadManager //as specifies the data type
             downloadID = downloadManager.enqueue(request)// enqueue puts the download request in the queue.
 
-            //notificationManager.sendNotification(CHANNEL_ID,"texting cunt", applicationContext)
+            //var downloadProgress: DownloadProgressUpdater.DownloadProgressListener
+            //downloadProgress = DownloadProgressUpdater(downloadManager, downloadID,downloadProgress)
+            getDownloadStatus(downloadManager,downloadID)
 
+
+            //notificationManager.sendNotification(CHANNEL_ID,"test", applicationContext)
 
         }else{
             //If no radioButton is selected, send a toast
@@ -118,59 +135,100 @@ class MainActivity : AppCompatActivity() {
         private const val CHANNEL_NAME = "Download Notifications"
 
     }
-}
 
-fun NotificationManager.sendNotification(channelId: String, messageBody: String, applicationContext: Context){
-
-    //Intent to make the notification clickable.
-    val contentIntent = Intent(applicationContext, MainActivity::class.java) //MainActivity specified as the destination for the intent.
-    val contentPendingIntent = PendingIntent.getActivity(
-        applicationContext,
-        NOTIFICATION_ID,
-        contentIntent,
-        PendingIntent.FLAG_UPDATE_CURRENT
-    )
-
-    //Intent for the button to do to the DetailActivity.
-    val detailIntent = Intent(applicationContext, DetailActivity::class.java)  //DetailActivity specified as the destination for the intent.
-
-    val pendingIntent: PendingIntent = PendingIntent.getActivity(
-        applicationContext,
-        REQUEST_CODE,
-        detailIntent,
-        PendingIntent.FLAG_UPDATE_CURRENT
-    )
-
-
-    val zipImage = BitmapFactory.decodeResource(
-        applicationContext.resources,
-        R.drawable.zip_file)
-
-
-    val builder = NotificationCompat.Builder(applicationContext, channelId)
-        .setSmallIcon(R.drawable.ic_assistant_black_24dp)
-        .setContentTitle(applicationContext.getString(R.string.notification_title))
-        .setContentText(messageBody)
-        .setLargeIcon(zipImage)
+    fun NotificationManager.sendNotification(channelId: String, messageBody: String, applicationContext: Context){
 
         //Intent to make the notification clickable.
-        .setContentIntent(contentPendingIntent)
-        .setAutoCancel(true) //Cancels the notification when clicked.
-
-        //Add button and the Intent for the button
-        .addAction(
-            R.drawable.ic_assistant_black_24dp,
-            "File details",
-            pendingIntent
+        val contentIntent = Intent(applicationContext, MainActivity::class.java) //MainActivity specified as the destination for the intent.
+        val contentPendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            NOTIFICATION_ID,
+            contentIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
         )
-        
-        //Additional style options for large image
-        .setStyle(NotificationCompat.BigPictureStyle()
-            .bigPicture(zipImage)
-            .bigLargeIcon(null))
+
+        //Intent for the button to do to the DetailActivity.
+        val detailIntent = Intent(applicationContext, DetailActivity::class.java)  //DetailActivity specified as the destination for the intent.
+        detailIntent.putExtra("fileName", selectedRadioButtonText) //Pass value with intent
+        detailIntent.putExtra("status", downloadResult) //Pass value with intent
+
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(
+            applicationContext,
+            REQUEST_CODE,
+            detailIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val zipImage = BitmapFactory.decodeResource(
+            applicationContext.resources,
+            R.drawable.zip_file)
+
+        val builder = NotificationCompat.Builder(applicationContext, channelId)
+            .setSmallIcon(R.drawable.ic_assistant_black_24dp)
+            .setContentTitle(applicationContext.getString(R.string.notification_title))
+            .setContentText(messageBody)
+            .setLargeIcon(zipImage)
+
+            //Intent to make the notification clickable.
+            .setContentIntent(contentPendingIntent)
+            .setAutoCancel(true) //Cancels the notification when clicked.
+
+            //Add button and the Intent for the button
+            .addAction(
+                R.drawable.ic_assistant_black_24dp,
+                "File details",
+                pendingIntent
+            )
+
+            //Additional style options for large image
+            .setStyle(NotificationCompat.BigPictureStyle()
+                .bigPicture(zipImage)
+                .bigLargeIcon(null))
 
         notify(0, builder.build())
+    }
 
+    fun getDownloadStatus(manager: DownloadManager, downloadId: Long) {
+        val query: DownloadManager.Query = DownloadManager.Query()
+        query.setFilterById(downloadId)
+
+        while (downloadStatus <= 2) {
+            Thread.sleep(500)
+
+            manager.query(query).use {
+                if (it.moveToFirst()) {
+
+                    totalBytes = it.getInt(it.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES))
+                    Log.i("Main Activity","Total bytes for this file is: $totalBytes Kbps")
+
+                    downloadStatus = it.getInt(it.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                    //Log.i("Main Activity","Download status is $downloadStatus")
+                    val bytesDownloadedSoFar = it.getInt(it.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR))
+                    Log.i("Main Activity","Bytes downloaded so far: $bytesDownloadedSoFar Kbps")
+
+                    when (downloadStatus){
+                        DownloadManager.STATUS_SUCCESSFUL -> {
+                            Log.i("Main Activity","Download successful")
+                            downloadResult = "Download successful"
+                        }
+                        DownloadManager.STATUS_FAILED -> {
+                            Log.i("Main Activity","Download failed")
+                            downloadResult = "Download failed"
+                        }
+                        DownloadManager.STATUS_RUNNING -> {
+                            Log.i("Main Activity", "Still downloading")
+                            //update progress
+                            val percentProgress = ((bytesDownloadedSoFar * 100L) / totalBytes)
+                            android.util.Log.i("Main Activity","Download progress: $percentProgress")
+                        }
+                    }
+                }
+            }
+        }
+
+        //Reset the download status once complete
+        downloadStatus = 0
+    }
 }
 
 private fun createChannel(channelId: String, channelName: String, notificationManager: NotificationManager) {
@@ -192,5 +250,51 @@ private fun createChannel(channelId: String, channelName: String, notificationMa
         notificationManager.createNotificationChannel(notificationChannel)
 
     }
-
 }
+
+class DownloadCompletedQuery(private val manager: DownloadManager, private val downloadId: Long) {
+    private val query: DownloadManager.Query = DownloadManager.Query()
+    var downloadStatus: Int = 0
+     private var totalBytes: Int = 0
+
+     init {
+        query.setFilterById(this.downloadId)
+        run()
+    }
+
+    @SuppressLint("Range") //Suppresses errors from code below.
+    fun run() {
+        while (downloadStatus <= 2) {
+            Thread.sleep(500)
+
+            manager.query(query).use {
+                if (it.moveToFirst()) {
+
+                    totalBytes = it.getInt(it.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES))
+                    Log.i("Main Activity","Total bytes for this file is: $totalBytes Kbps")
+
+                    downloadStatus = it.getInt(it.getColumnIndex(DownloadManager.COLUMN_STATUS))
+                    //Log.i("Main Activity","Download status is $downloadStatus")
+                    val bytesDownloadedSoFar = it.getInt(it.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR))
+                    Log.i("Main Activity","Bytes downloaded so far: $bytesDownloadedSoFar Kbps")
+
+                    when (downloadStatus){
+                        DownloadManager.STATUS_SUCCESSFUL -> {
+                            Log.i("Main Activity","Download successful")
+                        }
+                        DownloadManager.STATUS_FAILED -> {
+                            Log.i("Main Activity","Download failed")
+                        }
+                        DownloadManager.STATUS_RUNNING -> {
+                            Log.i("Main Activity", "Still downloading")
+                            //update progress
+                            val percentProgress = ((bytesDownloadedSoFar * 100L) / totalBytes)
+                            android.util.Log.i("Main Activity","Download progress: $percentProgress")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
